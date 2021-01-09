ed873a3aa089c85ebabdb6fc5e1d2406c4e597b9
zigakleine <ziga.kleine@gmail.com>
2021-01-09 15:26:31 +0100

    ok

--- featureExtraction.py
+++ featureExtraction.py
@@ -1,21 +1,21 @@
-# from tensorflow.keras.applications import vgg16
-# from tensorflow import keras
-#
-# class FeatureExtraction:
-#
-#     def __init__(self):
-#         self.IMG_SIZE = 224
-#         model_base = vgg16.VGG16(weights='imagenet', include_top=True, input_shape=(self.IMG_SIZE, self.IMG_SIZE, 3))
-#         self.model = keras.Model(inputs=model_base.layers[0].input, outputs=model_base.layers[-2].output)
-#         self.model.summary()
-#
-#     def extract_features(self, imgs):
-#
-#         feature_vectors = []
-#
-#         for img in imgs:
-#             img_expand = img.reshape(1, self.IMG_SIZE, self.IMG_SIZE, 3)
-#             res = self.model.predict(img_expand)
-#             feature_vectors.append(res[0])
-#
-#         return feature_vectors
+from tensorflow.keras.applications import vgg16
+from tensorflow import keras
+
+class FeatureExtraction:
+
+    def __init__(self):
+        self.IMG_SIZE = 224
+        model_base = vgg16.VGG16(weights='imagenet', include_top=True, input_shape=(self.IMG_SIZE, self.IMG_SIZE, 3))
+        self.model = keras.Model(inputs=model_base.layers[0].input, outputs=model_base.layers[-2].output)
+        self.model.summary()
+
+    def extract_features(self, imgs):
+
+        feature_vectors = []
+
+        for img in imgs:
+            img_expand = img.reshape(1, self.IMG_SIZE, self.IMG_SIZE, 3)
+            res = self.model.predict(img_expand)
+            feature_vectors.append(res[0])
+
+        return feature_vectors
--- kmeansClustering.py
+++ kmeansClustering.py
@@ -1,14 +1,14 @@
-# from sklearn.cluster import KMeans
-#
-#
-# class KMeansClustering:
-#
-#     def __init__(self):
-#         pass
-#
-#     def perform_clustering(self, feature_vectors, num_clusters):
-#
-#         kmeans = KMeans(n_clusters=num_clusters).fit(feature_vectors)
-#         labels = kmeans.labels_
-#
-#         return labels
+from sklearn.cluster import KMeans
+
+
+class KMeansClustering:
+
+    def __init__(self):
+        pass
+
+    def perform_clustering(self, feature_vectors, num_clusters):
+
+        kmeans = KMeans(n_clusters=num_clusters).fit(feature_vectors)
+        labels = kmeans.labels_
+
+        return labels
